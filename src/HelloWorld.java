707dd2bf205da992d3cc52ec04afcbd6a7c62637
OmerEngin <omerengin2909@gmail.com>
2022-04-01 09:02:14 +0300

    work 3 is done and about to be pushed to GitHub after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     //Doing some work here(you can any type you want)
 
     //Doing awesome eork 2 !
+
+    //Doing awesome work 3 !
+    // Awesome work 3 is done and ready to be committed
+    // And ready to be push to remote repository
 }
